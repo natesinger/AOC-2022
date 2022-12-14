826fe4886b306f97d46fb214dc5dd3f0de159510
Nathaniel Singer <nsinger@nuro.ai>
2022-12-13 18:48:22 -0800

    test

--- solutions/day-13/part2.py
+++ solutions/day-13/part2.py
@@ -1,144 +1,94 @@
 #!/usr/bin/python3
 import ast
 
-current_right = False
-correct_indicies = []
-
-done = False
-
-output = []
-
-def check_values(left, right, depth:int=1):
-    global current_right, done
-    
-    if done: return False
-    
-    if left == None:
-        current_right = True
-        done = True
-        return True
-    elif right == None:
-        done = True
-        return True
-        
-    elif type(left) == int and type(right) == int:
-        if left < right:
-            done = True
-            current_right = True
-            return True
-        
-        elif left > right:
-            done = True
-            return True
-    
-    elif type(left) == list and type(right) == list:
-        len_left = len(left)
-        len_right = len(right)
-        longest_length = len_left if len_left > len_right else len_right
-        
-        for i in range(longest_length):
-            try: tpm_left = left[i]
-            except IndexError: tpm_left = None
-            
-            try: tmp_right = right[i]
-            except IndexError: tmp_right = None
-            
-            if check_values(tpm_left, tmp_right, depth+1): return True
-    
-    # mixed case
-    elif type(left) == int and type(right) == list:
-        check_values([left], right, depth+1)
-        
-    elif type(left) == list and type(right) == int:
-        check_values(left, [right], depth+1)
-    
-    return False
-    
-
-with open('test-data-modified.txt','r') as fio:
-    for n, line in enumerate(fio):
-        set_number = n // 3
-        
-        if n % 3 == 0: first_line = line.strip()
-        elif n % 3 == 1: second_line = line.strip()
-        elif n % 3 == 2:       
-            first_line = ast.literal_eval(first_line)
-            second_line = ast.literal_eval(second_line)
-            
-            len_left = len(first_line)
-            len_right = len(second_line)
-            longest_length = len_left if len_left > len_right else len_right
-            
-            for i in range(longest_length):
-                try: left = first_line[i]
-                except IndexError: left = None
-                
-                try: right = second_line[i]
-                except IndexError: right = None
-                
-                if check_values(left, right): break
-                
-        
-            if current_right == True:
-                correct_indicies.append(set_number+1)
-                current_right = False
-                
-                output.append(first_line)
-                output.append(second_line)
-            else:
-                output.append(second_line)
-                output.append(first_line)
-            done = False
-
-
-for line_pos in range(len(output)):
-    first_line = output[line_pos]
-    second_line = output[line_pos+1]
-    
-    check_values(first_line, second_line)
-    print(current_right)
-    current_right = False
-    #exit()
-    
-    
-
 """
-for run in range(5):
-    input = output
-    output = []
+What are the indices of the pairs that are already in the right order? (The first pair has index 1, the second pair has index 2, and so on.) In the above example, the pairs in the right order are 1, 2, 4, and 6; the sum of these indices is 13.
+"""
+
+# processor function
+def process_pair(left, right, depth=0) -> str:
+    print(f"{depth * '  '}- Compare {left} vs {right}")
     
-    for n in range(len(input)):
-        try:
-            first_line = input[n]
-            second_line = input[n+1] 
-        except IndexError: exit()
-            
-        len_left = len(first_line)
-        len_right = len(second_line)
-        longest_length = len_left if len_left > len_right else len_right
+    match (left, right):
+        # both ints
+        case (int(), int()):
+            if left > right:
+                print(f"{(depth+1)*'  '}- Right side is smaller, so inputs are not in the right order")
+                return 'incorrect'
+            elif left < right:
+                print(f"{(depth+1)*'  '}- Left side is smaller, so inputs are in the right order")
+                return 'correct'
+            elif left == right:
+                return 'same'
         
-        for i in range(longest_length):
-            try: left = first_line[i]
-            except IndexError: left = None
+        # both lists
+        case (list(), list()):
+            len_left,len_right = len(left),len(right)
+            longer_length = len_left if len_left > len_right else len_right
             
-            try: right = second_line[i]
-            except IndexError: right = None
+            # start new jobs at one depth lower
+            for i in range(longer_length):
+                try: left[i]
+                except:
+                    print(f"{(depth+1)*'  '}- Left side ran out of items, so inputs are in the right order")
+                    return 'correct'
+                
+                try: right[i]
+                except:
+                    print(f"{(depth+1)*'  '}- Right side ran out of items, so inputs are not in the right order")
+                    return 'incorrect'
+                
+                result = process_pair(left[i], right[i], depth+1)
+                
+                if result == 'correct': return 'correct'
+                elif result == 'incorrect': return 'incorrect'
+                elif result == 'same': continue
+            return 'same'
+        
+        # mixed types
+        case (list(), int()):
+            print(f"{(depth)*'  '}- Mixed types; convert right to [{right}] and retry comparison")
+            result = process_pair(left, [right], depth)
+            if result == 'correct': return 'correct'
+            elif result == 'incorrect': return 'incorrect'
+            elif result == 'same': return 'same'
             
-            if check_values(left, right): break
-            
+        case (int(), list()):
+            print(f"{(depth)*'  '}- Mixed types; convert left to [{left}] and retry comparison")
+            result = process_pair([left], right, depth)
+            if result == 'correct': return 'correct'
+            elif result == 'incorrect': return 'incorrect'
+            elif result == 'same': return 'same'
+
+
+# retreive data
+with open('input.txt','rt') as fio:
+    input_data = [ line.strip() for line in fio ]
+
+
+# solve part 1
+correct_indicides = []
+
+for n,line in enumerate(input_data):
     
-        if current_right == True:
-            current_right = False
-            
-            output.append(first_line)
-            output.append(second_line)
+    # pull three lines and if its the third line, process
+    if n % 3 == 2:
+        pair_count = n // 3
+        first_line = ast.literal_eval(input_data[n-2])
+        second_line = ast.literal_eval(input_data[n-1])
+        
+        # process
+        print(f"== Pair {pair_count+1} ==")
+        result = process_pair(first_line, second_line)
+        
+        if result == 'correct': 
+            correct_indicides.append(pair_count+1)
+        elif result == 'incorrect':
+            pass
         else:
-            output.append(second_line)
-            output.append(first_line)
-    
+            print("something weird happened 5")
+            exit()
+            
+        print()
 
-for l in output: print(l)
-print()        
-    
-"""
-
+print(f"Answer: {sum(correct_indicides)}")
